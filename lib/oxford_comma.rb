0f599008d3e8edc5cc31fb1a7d8ee8a73549a669
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 19:02:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
 # if there is one element in a array, return string
-# first we need to count elemets in an array
-if array.length == 1, return array
+# we need to count elemets in an array
+if '#{array}'.length == 1, return array
 # if there is two elements, add and between them
-elsif array.length == 2, return array.split("and")
-#if there are three elements, add commas and a final and
-elsif array.length == 3, return array.split()
+# elsif array.length == 2, return array.split("and")
+# #if there are three elements, add commas and a final and
+# elsif array.length == 3, return array.split()
 
 #correctly formats arrays of lengths greater then three
 #commas, between every word then the final , and
