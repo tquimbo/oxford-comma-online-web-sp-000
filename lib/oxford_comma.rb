703f1a8fe72153e2151e5ef1a565d34de563e657
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 19:00:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-"hippo,giraffe,monkey,horse".split("and")
+# if there is one element in a array, return string
+# first we need to count elemets in an array
+if array.length == 1, return array
+# if there is two elements, add and between them
+elsif array.length == 2, return array.split("and")
+#if there are three elements, add commas and a final and
+elsif array.length == 3, return array.split()
+  
+#correctly formats arrays of lengths greater then three 
+#commas, between every word then the final , and 
+
 end
+end
+
+
+
+#
