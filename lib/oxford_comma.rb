67c5ee36298332dabac53492577d58192962fee7
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 20:06:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -12,8 +12,8 @@ elsif new_array.count() == 2
 # #if there are three elements, add commas and a final and last element in an array you ,and
 # elsif array.length == 3, return array.split()
 elsif new_array.count() >= 3
+  new_array.join(", ")
   new_array.insert(-2, "and")
- return new_array.join(", ")
 #correctly formats arrays of lengths greater then three
 #commas, between every word then the final , and
 
