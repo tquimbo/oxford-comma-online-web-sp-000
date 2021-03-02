57994bd7d29d309def2ef97c5612907ed71901ef
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 19:25:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ new_array = array
 #how to get the array elemets to use in my code
 # if there is one element in a array, return string
 # we need to count elemets in an array
- if new_array.length == 1 return new_array.join
+ return new_array.join
 # if there is two elements, add and between them
 # elsif array.length == 2, return array.join("and")
 # #if there are three elements, add commas and a final and last element in an array you ,and
